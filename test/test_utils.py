461ff104a76c6d09c3f4270d4b9f2be1c8f6e6e5
F-G Fernandez <fgfm03@hotmail.fr>
2019-09-20 21:00:04 +0200

    Project setup (#1)
    * docs: Updated readme
    
    * docs: Added contribution guidelines
    
    * feat: Added utils module
    
    Added environment information script for issuers.
    
    * chore: Added package setup
    
    * docs: Updated readme
    
    Added installation instructions
    
    * docs: Added issue templates
    
    * chore: Added CI
    
    * test: Added unittests for utils
    
    * test: Added unittests into CI
    
    * docs: Updated readme
    
    Added CI and coverage badges
    
    * refactor: Removed unused import

---
+++ test/test_utils.py
@@ -0,0 +1,12 @@
+import unittest
+from pyronear.utils.collect_env import get_pretty_env_info
+
+
+class TestCollectEnv(unittest.TestCase):
+    def test_smoke(self):
+        info_output = get_pretty_env_info()
+        self.assertTrue(info_output.count('\n') >= 19)
+
+
+if __name__ == '__main__':
+    unittest.main()
