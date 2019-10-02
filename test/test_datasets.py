dc750ebbbe28b64c53f437aef9750da5b8a5a2c8
F-G Fernandez <fgfm03@hotmail.fr>
2019-10-02 18:35:44 +0200

    test: Refactored datasets unittests

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -8,30 +8,22 @@ from pyronear import datasets
 
 
 class TestCollectEnv(unittest.TestCase):
-    def test_downloadurl(self):
-        # URL
-        wrong_url1 = 'url'
-        wrong_url2 = 0
+    def test_downloadurl(self, root='/tmp'):
+        # Valid input
         url = 'https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt'
 
-        # Root
-        wrong_root = 0
-        root = '/tmp'
-
         # URL error cases
-        self.assertRaises(ValueError, datasets.utils.download_url, wrong_url1, root, verbose=False)
-        self.assertRaises(TypeError, datasets.utils.download_url, wrong_url2, root, verbose=False)
+        self.assertRaises(ValueError, datasets.utils.download_url, 'url', root, verbose=False)
+        self.assertRaises(TypeError, datasets.utils.download_url, 0, root, verbose=False)
 
         # Root error cases
-        self.assertRaises(TypeError, datasets.utils.download_url, url, wrong_root, verbose=False)
+        self.assertRaises(TypeError, datasets.utils.download_url, url, 0, verbose=False)
 
         # Working case
         datasets.utils.download_url(url, root, verbose=True)
         self.assertTrue(Path(root, url.rpartition('/')[-1]).is_file())
 
-    def test_openfire(self):
-
-        root = '/tmp'
+    def test_openfire(self, root='/tmp'):
 
         # Working case
         # Check inherited properties
