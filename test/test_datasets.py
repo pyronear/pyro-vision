29821a664aa635ae6f63228a311d9ecb385bc158
F-G Fernandez <fgfm03@hotmail.fr>
2019-10-08 20:29:07 +0200

    test: Moved temporary path to context

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -8,40 +8,42 @@ from pyronear import datasets
 
 
 class TestCollectEnv(unittest.TestCase):
-    def test_downloadurl(self, root='/tmp'):
+    def test_downloadurl(self):
         # Valid input
         url = 'https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt'
 
-        # URL error cases
-        self.assertRaises(ValueError, datasets.utils.download_url, 'url', root, verbose=False)
-        self.assertRaises(TypeError, datasets.utils.download_url, 0, root, verbose=False)
+        with Path('/tmp') as root:
+            # URL error cases
+            self.assertRaises(ValueError, datasets.utils.download_url, 'url', root, verbose=False)
+            self.assertRaises(TypeError, datasets.utils.download_url, 0, root, verbose=False)
 
-        # Root error cases
-        self.assertRaises(TypeError, datasets.utils.download_url, url, 0, verbose=False)
+            # Root error cases
+            self.assertRaises(TypeError, datasets.utils.download_url, url, 0, verbose=False)
 
-        # Working case
-        datasets.utils.download_url(url, root, verbose=True)
-        self.assertTrue(Path(root, url.rpartition('/')[-1]).is_file())
+            # Working case
+            datasets.utils.download_url(url, root, verbose=True)
+            self.assertTrue(Path(root, url.rpartition('/')[-1]).is_file())
 
-    def test_openfire(self, root='/tmp'):
+    def test_openfire(self):
 
-        # Working case
-        # Check inherited properties
-        dataset = datasets.OpenFire(root=root, train=True, download=True)
-        self.assertTrue(isinstance(dataset, VisionDataset))
+        with Path('/tmp') as root:
+            # Working case
+            # Check inherited properties
+            dataset = datasets.OpenFire(root=root, train=True, download=True)
+            self.assertTrue(isinstance(dataset, VisionDataset))
 
-        # Check against number of samples in extract
-        datasets.utils.download_url(dataset.url, root, filename='extract.json', verbose=False)
-        with open(Path(root, 'extract.json'), 'rb') as f:
-            extract = json.load(f)
-        # Uncomment when download issues are resolved
-        # self.assertEqual(len(dataset), len(extract))
+            # Check against number of samples in extract
+            datasets.utils.download_url(dataset.url, root, filename='extract.json', verbose=False)
+            with open(root.joinpath('extract.json'), 'rb') as f:
+                extract = json.load(f)
+            # Uncomment when download issues are resolved
+            # self.assertEqual(len(dataset), len(extract))
 
-        # Check integrity of samples
-        img, target = dataset[0]
-        self.assertTrue(isinstance(img, Image))
-        self.assertTrue(isinstance(target, int))
-        self.assertEqual(dataset.class_to_idx[extract[0]['target']], target)
+            # Check integrity of samples
+            img, target = dataset[0]
+            self.assertTrue(isinstance(img, Image))
+            self.assertTrue(isinstance(target, int))
+            self.assertEqual(dataset.class_to_idx[extract[0]['target']], target)
 
 
 if __name__ == '__main__':
