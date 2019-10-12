96e2cee00d8d24fe8b51fc19e5af2a9d8ac0a0e3
F-G Fernandez <fgfm03@hotmail.fr>
2019-10-12 12:18:20 +0200

    test: Updated utils import

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,10 +1,10 @@
 import unittest
-from pyronear.utils.collect_env import get_pretty_env_info
+from pyronear import utils
 
 
 class TestCollectEnv(unittest.TestCase):
     def test_prettyenv(self):
-        info_output = get_pretty_env_info()
+        info_output = utils.get_pretty_env_info()
         self.assertTrue(info_output.count('\n') >= 19)
 
 
