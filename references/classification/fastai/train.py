9ef36dde8825767886659d44df085c8503992833
F-G Fernandez <fgfm03@hotmail.fr>
2019-10-16 16:49:11 +0200

    style: Renamed learner

--- references/classification/fastai/train.py
+++ references/classification/fastai/train.py
@@ -33,17 +33,17 @@ def main(args):
     il = vision.ImageList.from_df(df, path=args.data_path).split_from_df('is_valid').label_from_df(cols='label')
     data = il.transform(vision.get_transforms(), size=args.resize).databunch(bs=args.batch_size, num_workers=args.workers).normalize(vision.imagenet_stats)
 
-    learn = vision.cnn_learner(data, vision.models.__dict__[args.model],
+    learner = vision.cnn_learner(data, vision.models.__dict__[args.model],
                                pretrained=args.pretrained,
                                wd=args.weight_decay,
                                ps=args.dropout_prob,
                                concat_pool=args.concat_pool,
                                metrics=vision.error_rate)
 
-    learn.fit_one_cycle(args.epochs, max_lr=slice(None, args.lr, None),
+    learner.fit_one_cycle(args.epochs, max_lr=slice(None, args.lr, None),
                         div_factor=args.div_factor)
 
-    learn.save(args.checkpoint)
+    learner.save(args.checkpoint)
 
 
 if __name__ == "__main__":
