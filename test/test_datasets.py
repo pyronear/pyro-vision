02229b4cf4a220710daaff1bf8cababca44e0640
F-G Fernandez <fgfm03@hotmail.fr>
2019-10-23 00:38:56 +0200

    test: Fixed tester naming

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -9,7 +9,7 @@ from torchvision.datasets import VisionDataset
 from pyronear import datasets
 
 
-class TestCollectEnv(unittest.TestCase):
+class DatasetsTester(unittest.TestCase):
     def test_downloadurl(self):
         # Valid input
         url = 'https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt'
--- test/test_utils.py
+++ test/test_utils.py
@@ -2,7 +2,7 @@ import unittest
 from pyronear import utils
 
 
-class TestCollectEnv(unittest.TestCase):
+class UtilsTester(unittest.TestCase):
     def test_prettyenv(self):
         info_output = utils.get_pretty_env_info()
         self.assertTrue(info_output.count('\n') >= 19)
