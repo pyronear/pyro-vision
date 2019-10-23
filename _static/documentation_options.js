1813a8d84d6006f904bd330ddf997535261612c8
k2-s0 <56176553+k2-s0@users.noreply.github.com>
2019-10-23 19:14:31 +0000

    [skip ci] Updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0rc1+5989ee9-git',
+    VERSION: '0.1.0rc1+d02e4e6-git',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
