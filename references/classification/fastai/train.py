73d107352b3eb62931d511fbe2eb4649603d03a2
F-G Fernandez <fgfm03@hotmail.fr>
2019-10-23 09:52:26 +0200

    fix: Fixed training scripts (#30)
    * fix: Fixed layer freezing
    
    Pure pytorch reference script had the model fully unfrozen by default which would not be comparable to the one using fastai. See #3
    
    * feat: Added options for model unfreezing
    
    * style: Corrected whitespaces and indentation
    
    * refactor: Switched to TTA
    
    Test Time Augmentation is now similar to training augmentation.
    
    * feat: Added resume option on references
    
    * style: Clarified argument description
    
    * fix: Moved saving message display
    
    Saving message should only be displayed if the checkpoint is actually being created.
    
    * docs: Updated docstrings
    
    * feat: Added binary classification option
    
    See #19
    
    * style: Fixed pep8 compliance
    
    * fix: Fixed argument description
    
    * fix: Fixed training script saving message

--- references/classification/fastai/train.py
+++ references/classification/fastai/train.py
@@ -40,19 +40,23 @@ def main(args):
 
     df = pd.DataFrame.from_dict(dict(name=fnames, label=labels, is_valid=is_valid))
 
-
     il = vision.ImageList.from_df(df, path=args.data_path).split_from_df('is_valid').label_from_df(cols='label')
-    data = il.transform(vision.get_transforms(), size=args.resize).databunch(bs=args.batch_size, num_workers=args.workers).normalize(vision.imagenet_stats)
+    il = il.transform(vision.get_transforms(), size=args.resize)
+    data = il.databunch(bs=args.batch_size, num_workers=args.workers).normalize(vision.imagenet_stats)
 
     learner = vision.cnn_learner(data, vision.models.__dict__[args.model],
-                               pretrained=args.pretrained,
-                               wd=args.weight_decay,
-                               ps=args.dropout_prob,
-                               concat_pool=args.concat_pool,
-                               metrics=vision.error_rate)
+                                 pretrained=args.pretrained,
+                                 wd=args.weight_decay,
+                                 ps=args.dropout_prob,
+                                 concat_pool=args.concat_pool,
+                                 metrics=vision.error_rate)
+    if args.resume:
+        learner.load(args.resume)
+    if args.unfreeze:
+        learner.unfreeze()
 
     learner.fit_one_cycle(args.epochs, max_lr=slice(None, args.lr, None),
-                        div_factor=args.div_factor)
+                          div_factor=args.div_factor)
 
     learner.save(args.checkpoint)
 
@@ -71,21 +75,20 @@ if __name__ == "__main__":
                         help='number of data loading workers (default: 16)')
     parser.add_argument('--lr', default=3e-3, type=float, help='initial learning rate')
     parser.add_argument("--concat-pool", dest="concat_pool",
-        help="Use pre-trained models from the modelzoo",
-        action="store_true"
-    )
+                        help="Use pre-trained models from the modelzoo",
+                        action="store_true")
     parser.add_argument('--dropout-prob', default=0.5, type=float, help='dropout rate of last FC layer')
     parser.add_argument('--wd', '--weight-decay', default=1e-2, type=float,
                         metavar='W', help='weight decay (default: 1e-2)',
                         dest='weight_decay')
     parser.add_argument('--div-factor', default=25., type=float, help='div factor of OneCycle policy')
     parser.add_argument('--checkpoint', default='checkpoint', type=str, help='name of output file')
-    parser.add_argument(
-        "--pretrained",
-        dest="pretrained",
-        help="Use pre-trained models from the modelzoo",
-        action="store_true",
-    )
+    parser.add_argument('--resume', default=None, help='checkpoint name to resume from (default: None)')
+    parser.add_argument("--unfreeze", dest="unfreeze", help="Should all layers be unfrozen",
+                        action="store_true")
+    parser.add_argument("--pretrained", dest="pretrained",
+                        help="Use pre-trained models from the modelzoo",
+                        action="store_true")
     args = parser.parse_args()
 
     main(args)
--- references/classification/torch/train.py
+++ references/classification/torch/train.py
@@ -41,9 +41,10 @@ def train_batch(model, x, target, optimizer, criterion):
     """Train a model for one iteration
     Args:
         model (torch.nn.Module): model to train
-        loader_iter (iter(torch.utils.data.DataLoader)): training dataloader iterator
+        x (torch.Tensor): input sample
+        target (torch.Tensor): output target
         optimizer (torch.optim.Optimizer): parameter optimizer
-        criterion (torch.nn.Module): criterion object
+        criterion (torch.nn.Module): loss used for backpropagation
     Returns:
         batch_loss (float): training loss
     """
@@ -63,7 +64,7 @@ def train_batch(model, x, target, optimizer, criterion):
 
 
 def train_epoch(model, train_loader, optimizer, criterion, master_bar,
-                epoch=0, scheduler=None, device='cpu'):
+                epoch=0, scheduler=None, device='cpu', bin_classif=False):
     """Train a model for one epoch
     Args:
         model (torch.nn.Module): model to train
@@ -74,6 +75,7 @@ def train_epoch(model, train_loader, optimizer, criterion, master_bar,
         epoch (int): current epoch index
         scheduler (torch.optim._LRScheduler, optional): learning rate scheduler
         device (str): device hosting tensor data
+        bin_classif (bool, optional): should the target be considered as binary
     Returns:
         batch_loss (float): latch batch loss
     """
@@ -85,6 +87,8 @@ def train_epoch(model, train_loader, optimizer, criterion, master_bar,
     for _ in progress_bar(range(len(train_loader)), parent=master_bar):
 
         x, target = next(loader_iter)
+        if bin_classif:
+            target = target.to(dtype=torch.float).view(-1, 1)
         if device.startswith('cuda'):
             x, target = x.cuda(non_blocking=True), target.cuda(non_blocking=True)
 
@@ -100,13 +104,14 @@ def train_epoch(model, train_loader, optimizer, criterion, master_bar,
     return train_loss
 
 
-def evaluate(model, test_loader, criterion, device='cpu'):
+def evaluate(model, test_loader, criterion, device='cpu', bin_classif=False):
     """Evaluation a model on a dataloader
     Args:
         model (torch.nn.Module): model to train
-        train_loader (torch.utils.data.DataLoader): validation dataloader
+        test_loader (torch.utils.data.DataLoader): validation dataloader
         criterion (torch.nn.Module): criterion object
         device (str): device hosting tensor data
+        bin_classif (bool, optional): should the target be considered as binary
     Returns:
         val_loss (float): validation loss
         acc (float): top1 accuracy
@@ -115,6 +120,8 @@ def evaluate(model, test_loader, criterion, device='cpu'):
     val_loss, correct, targets = 0, 0, 0
     with torch.no_grad():
         for x, target in test_loader:
+            if bin_classif:
+                target = target.to(dtype=torch.float).view(-1, 1)
             # Work with tensors on GPU
             if device.startswith('cuda'):
                 x, target = x.cuda(), target.cuda()
@@ -123,7 +130,11 @@ def evaluate(model, test_loader, criterion, device='cpu'):
             outputs = model.forward(x)
             val_loss += criterion(outputs, target).item()
             # Index of max log-probability
-            pred = outputs.max(1, keepdim=True)[1]
+            if bin_classif:
+                pred = torch.sigmoid(outputs).round()
+            else:
+                pred = outputs.argmax(1, keepdim=True)
+
             correct += pred.eq(target.view_as(pred)).sum().item()
             targets += x.size(0)
     val_loss /= len(test_loader)
@@ -147,7 +158,7 @@ def main(args):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
-    train_transforms = transforms.Compose([
+    data_transforms = transforms.Compose([
         transforms.RandomResizedCrop((args.resize, args.resize)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(10),
@@ -155,27 +166,26 @@ def main(args):
         normalize
     ])
 
-    test_transforms = transforms.Compose([
-        transforms.Resize((args.resize, args.resize)),
-        transforms.ToTensor(),
-        normalize
-    ])
-
     # Train & test sets
     train_set = OpenFire(root=args.data_path, train=True, download=True,
-                         transform=train_transforms)
+                         transform=data_transforms)
     val_set = OpenFire(root=args.data_path, train=False, download=True,
-                       transform=test_transforms)
+                       transform=data_transforms)
     num_classes = len(train_set.classes)
+    if args.binary:
+        if num_classes == 2:
+            num_classes = 1
+        else:
+            raise ValueError('unable to cast number of classes to binary setting')
     # Samplers
     train_sampler = torch.utils.data.RandomSampler(train_set)
     test_sampler = torch.utils.data.SequentialSampler(val_set)
 
     # Data loader
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, sampler=train_sampler,
-                                             num_workers=args.workers, pin_memory=True)
+                                               num_workers=args.workers, pin_memory=True)
     test_loader = torch.utils.data.DataLoader(val_set, batch_size=args.batch_size, sampler=test_sampler,
-                                            num_workers=args.workers, pin_memory=True)
+                                              num_workers=args.workers, pin_memory=True)
 
     # Model definition
     model = torchvision.models.__dict__[args.model](pretrained=args.pretrained)
@@ -183,10 +193,26 @@ def main(args):
     # Change fc
     in_features = getattr(model, 'fc').in_features
     setattr(model, 'fc', nn.Linear(in_features, num_classes))
+
+    # Resume
+    if args.resume:
+        model.load_state_dict(torch.load(args.resume)['model'])
+
+    # Freeze layers
+    if not args.unfreeze:
+        # Model is sequential
+        for n, p in model.named_parameters():
+            if not n.startswith('fc'):
+                p.requires_grad = False
+
+    # Send to device
     model.to(args.device)
 
     # Loss function
-    criterion = nn.CrossEntropyLoss()
+    if args.binary:
+        criterion = nn.BCEWithLogitsLoss()
+    else:
+        criterion = nn.CrossEntropyLoss()
 
     # optimizer
     optimizer = optim.Adam(model.parameters(),
@@ -195,9 +221,9 @@ def main(args):
 
     # Scheduler
     lr_scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr,
-                                              epochs=args.epochs, steps_per_epoch=len(train_loader),
-                                              cycle_momentum=(not isinstance(optimizer, optim.Adam)),
-                                              div_factor=args.div_factor, final_div_factor=args.final_div_factor)
+                                                 epochs=args.epochs, steps_per_epoch=len(train_loader),
+                                                 cycle_momentum=(not isinstance(optimizer, optim.Adam)),
+                                                 div_factor=args.div_factor, final_div_factor=args.final_div_factor)
 
     best_loss = math.inf
     mb = master_bar(range(args.epochs))
@@ -205,25 +231,27 @@ def main(args):
         # Training
         train_loss = train_epoch(model, train_loader, optimizer, criterion,
                                  master_bar=mb, epoch=epoch_idx, scheduler=lr_scheduler,
-                                 device=args.device)
+                                 device=args.device, bin_classif=args.binary)
 
         # Evaluation
-        val_loss, acc = evaluate(model, test_loader, criterion, device=args.device)
+        val_loss, acc = evaluate(model, test_loader, criterion, device=args.device,
+                                 bin_classif=args.binary)
 
         mb.first_bar.comment = f"Epoch {epoch_idx+1}/{args.epochs}"
-        mb.write(f'Epoch {epoch_idx+1}/{args.epochs} - Training loss: {train_loss:.4} | Validation loss: {val_loss:.4} | Error rate: {1 - acc:.4}')
+        mb.write(f"Epoch {epoch_idx+1}/{args.epochs} - Training loss: {train_loss:.4} | "
+                 f"Validation loss: {val_loss:.4} | Error rate: {1 - acc:.4}")
 
         # State saving
         if val_loss < best_loss:
-            print(f"Validation loss decreased {best_loss:.4} --> {val_loss:.4}: saving state...")
-            best_loss = val_loss
             if args.output_dir:
+                print(f"Validation loss decreased {best_loss:.4} --> {val_loss:.4}: saving state...")
                 torch.save(dict(model=model.state_dict(),
                                 optimizer=optimizer.state_dict(),
                                 lr_scheduler=lr_scheduler.state_dict(),
                                 epoch=epoch_idx,
                                 args=args),
                            Path(args.output_dir, f"{args.checkpoint}.pth"))
+            best_loss = val_loss
 
 
 if __name__ == "__main__":
@@ -246,23 +274,21 @@ if __name__ == "__main__":
                         dest='weight_decay')
     parser.add_argument('--div-factor', default=25., type=float, help='div factor of OneCycle policy')
     parser.add_argument('--final-div-factor', default=1e4, type=float, help='final div factor of OneCycle policy')
-    parser.add_argument('--output-dir', default=None, help='path where to save')
-    parser.add_argument('--checkpoint', default='checkpoint', type=str, help='name of output file')
-    parser.add_argument('--resume', default='', help='resume from checkpoint')
+    parser.add_argument('--output-dir', default=None, help='path for output saving')
+    parser.add_argument('--checkpoint', default=None, type=str, help='name of output file')
+    parser.add_argument('--resume', default=None, help='checkpoint file to resume from (default: None)')
+    parser.add_argument("--binary", dest="binary", help="Should the task be considered as binary Classification",
+                        action="store_true")
     parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                         help='start epoch')
-    parser.add_argument(
-        "--deterministic",
-        dest="deterministic",
-        help="Should the training be performed in deterministic mode",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--pretrained",
-        dest="pretrained",
-        help="Use pre-trained models from the modelzoo",
-        action="store_true",
-    )
+    parser.add_argument("--unfreeze", dest="unfreeze", help="Should all layers be unfrozen",
+                        action="store_true")
+    parser.add_argument("--deterministic", dest="deterministic",
+                        help="Should the training be performed in deterministic mode",
+                        action="store_true")
+    parser.add_argument("--pretrained", dest="pretrained",
+                        help="Use pre-trained models from the modelzoo",
+                        action="store_true")
     args = parser.parse_args()
 
     main(args)
