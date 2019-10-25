80b9d693e535b8002059cf138d827c6ab904f66b
F-G Fernandez <fgfm03@hotmail.fr>
2019-10-25 22:01:35 +0200

    fix: Fixed concatenated pooling

--- pyronear/nn/functional.py
+++ pyronear/nn/functional.py
@@ -3,18 +3,18 @@
 
 # Based on https://github.com/fastai/fastai/blob/master/fastai/layers.py
 
+import torch
 import torch.nn.functional as F
 
 
-def adaptive_concat_pool2d(input, output_size, return_indices=False):
+def adaptive_concat_pool2d(input, output_size):
     """Concatenates a 2D adaptive max pooling and a 2D adaptive average pooling
     over an input signal composed of several input planes.
     See :class:`~torch.nn.AdaptiveConcatPool2d` for details and output shape.
     Args:
         output_size: the target output size (single integer or
             double-integer tuple)
-        return_indices: whether to return pooling indices. Default: ``False``
     """
 
-    return torch.cat([F.adaptive_max_pool2d(input, output_size, return_indices),
-                      F.adaptive_avg_pool2d(input, output_size, return_indices)], dim=1)
+    return torch.cat([F.adaptive_max_pool2d(input, output_size),
+                      F.adaptive_avg_pool2d(input, output_size)], dim=1)
