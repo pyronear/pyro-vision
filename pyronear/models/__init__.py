56294a11fd5ed810976b4d1368bcf08f77ab4e6d
F-G Fernandez <fgfm03@hotmail.fr>
2019-10-25 22:20:07 +0200

    test: Updated test_models

--- pyronear/models/__init__.py
+++ pyronear/models/__init__.py
@@ -1,1 +1,1 @@
-from .resnet import *
+from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
--- test/test_models.py
+++ test/test_models.py
@@ -1,29 +1,44 @@
 import unittest
 import torch
+import numpy as np
+import random
 from pyronear import models
 
 
+def set_rng_seed(seed):
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+
+def get_available_classification_models():
+    # TODO add a registration mechanism to torchvision.models
+    return [k for k, v in models.__dict__.items() if callable(v) and k[0].lower() == k[0] and k[0] != "_"]
+
+
 class ModelsTester(unittest.TestCase):
-    def test_resnet(self):
 
-        # Test parameters
-        img_shape = (3, 224, 224)
-        bin_classif = True
-
-        # Generated inputs
-        img_tensor = torch.rand(img_shape)
-        num_classes = 1 if bin_classif else None
-
-        # Non-binary classification
-        self.assertRaises(NotImplementedError, models.resnet,
-                          depth=18, bin_classif=False)
-
-        # Working case
-        model = models.resnet(depth=18, bin_classif=bin_classif).eval()
+    def _test_classification_model(self, name, input_shape):
+        # passing num_class equal to a number other than 1000 helps in making the test
+        # more enforcing in nature
+        set_rng_seed(0)
+        model = models.__dict__[name](num_classes=50)
+        model.eval()
+        x = torch.rand(input_shape)
         with torch.no_grad():
-            out = model(img_tensor.unsqueeze(0))
+            out = model(x)
+        # self.assertExpected(out, rtol=1e-2, atol=0.)
+        self.assertEqual(out.shape[-1], 50)
 
-        self.assertEqual(out.shape, (1, num_classes))
+
+for model_name in get_available_classification_models():
+    # for-loop bodies don't define scopes, so we have to save the variables
+    # we want to close over in some way
+    def do_test(self, model_name=model_name):
+        input_shape = (1, 3, 224, 224)
+        self._test_classification_model(model_name, input_shape)
+
+    setattr(ModelsTester, "test_" + model_name, do_test)
 
 
 if __name__ == '__main__':
