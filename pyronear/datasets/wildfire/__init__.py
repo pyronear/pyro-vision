ed91e9000e872a8b6227795cd773777065993ec9
x0s <guillaume.fidanza@gmail.com>
2019-12-15 13:39:51 +0100

    Add WildFire Dataset object and split strategies

--- pyronear/datasets/wildfire/__init__.py
+++ pyronear/datasets/wildfire/__init__.py
@@ -1,1 +1,5 @@
 from .fire_labeler import FireLabeler
+from .split_strategy import (SplitStrategy,
+                             ExhaustSplitStrategy)
+from .wildfire import (WildFireDataset,
+                       WildFireSplitter)
