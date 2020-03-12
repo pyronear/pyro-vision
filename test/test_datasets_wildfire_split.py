8e6cc84629e548ec4c572486d90d226416e678ff
x0s <guillaume.fidanza@gmail.com>
2020-03-12 10:28:08 +0100

    Comply with standards/flake8

--- test/test_datasets_wildfire_split.py
+++ test/test_datasets_wildfire_split.py
@@ -30,13 +30,13 @@ class WildFireDatasetTester(unittest.TestCase):
 
         # try to get one image of wildfire (item 3 is authorized image fixture)
         observation_3, metadata_3 = wildfire[3]
-        self.assertIsInstance(observation_3, np.ndarray) # image correctly loaded ?
-        self.assertIsInstance(metadata_3, pd.Series) # metadata correctly loaded ?
+        self.assertIsInstance(observation_3, np.ndarray)  # image correctly loaded ?
+        self.assertIsInstance(metadata_3, pd.Series)  # metadata correctly loaded ?
 
     def test_invalid_csv_path_raises_exception(self):
         with self.assertRaises(ValueError):
-            wildfire = WildFireDataset(metadata='bad_path.csv',
-                                       path_to_frames=self.path_to_frames)
+            WildFireDataset(metadata='bad_path.csv',
+                            path_to_frames=self.path_to_frames)
 
 
 class WildFireDatasetSplitter(unittest.TestCase):
@@ -63,25 +63,26 @@ wildfire_loader_test = DataLoader(splitter.test, batch_size=64, shuffle=True)
                                         path_to_frames=self.path_to_frames)
 
     def test_consistent_ratios_good_init(self):
-        ratios = {'train': 0.7, 'val': 0.15, 'test':0.15}
-        splitter =  WildFireSplitter(ratios)
+        ratios = {'train': 0.7, 'val': 0.15, 'test': 0.15}
+        splitter = WildFireSplitter(ratios)
         self.assertEqual(ratios, splitter.ratios)
 
     def test_inconsistent_ratios_raise_exception(self):
-        ratios = {'train': 0.9, 'val': 0.2, 'test':0.1}  # sum > 1
+        ratios = {'train': 0.9, 'val': 0.2, 'test': 0.1}  # sum > 1
         with self.assertRaises(ValueError):
-            splitter =  WildFireSplitter(ratios)
+            WildFireSplitter(ratios)
 
     def test_splitting_gives_good_splits_size(self):
         n_samples_expected = {'train': 685, 'val': 147, 'test': 142}
-        ratios = {'train': 0.7, 'val': 0.15, 'test':0.15}
+        ratios = {'train': 0.7, 'val': 0.15, 'test': 0.15}
 
-        splitter =  WildFireSplitter(ratios)
+        splitter = WildFireSplitter(ratios)
         splitter.fit(self.wildfire)
 
         self.assertEqual(splitter.n_samples_, n_samples_expected)
         for (set_, ratio_) in splitter.ratios_.items():
             self.assertAlmostEqual(ratio_, ratios[set_], places=2)
+
 
 if __name__ == '__main__':
     unittest.main()
