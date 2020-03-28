97e48d1ce3b64ea15e0f79b953149e7007bd561d
x0s <guillaume.fidanza@gmail.com>
2020-03-28 13:50:08 +0100

    Load Image default to PIL instead of Tensor since some transforms requires a PIL

--- pyronear/datasets/utils.py
+++ pyronear/datasets/utils.py
@@ -17,12 +17,14 @@ from torchvision.datasets.utils import check_integrity
 class VisionMixin:
     """Class to provide re-usabled functions to classes dealing this Vision (ie: VisionDataset)"""
     @staticmethod
-    def load_image(path):
-        """Load an image from a path into a pytorch Tensor"""
+    def load_image(path, to_tensor=False):
+        """Load an image from a path into a (PIL Image) or a (pytorch Tensor)"""
         # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
         with open(path, 'rb') as f:
-            img = Image.open(f)
-            return transforms.ToTensor()(img.convert('RGB'))
+            img = (Image.open(f)
+                        .convert('RGB'))
+            return transforms.ToTensor()(img) if to_tensor else img
+
 
 
 def url_retrieve(url, outfile, timeout=4):
