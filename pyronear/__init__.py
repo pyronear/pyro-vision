b48b37afa0ea68b40e96daaaf0596fd9039ed792
F-G Fernandez <fgfm03@hotmail.fr>
2020-04-30 15:54:09 +0200

    fix: Fixed version attribute import
    Catch the import error of the version attribute to avoid failures for dev. Closes #49

--- pyronear/__init__.py
+++ pyronear/__init__.py
@@ -1,4 +1,8 @@
-from .version import __version__
 from pyronear import datasets
 from pyronear import models
 from pyronear import utils
+
+try:
+    from .version import __version__  # noqa: F401
+except ImportError:
+    pass
