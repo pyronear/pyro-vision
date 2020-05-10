09d0bc7b5ea678d40d74bc6c18f06bd60b437096
matéo lostanlen <mateo.lostanlen@gmail.com>
2020-05-10 16:15:12 +0200

    clean code in subsampler dataset

--- pyronear/datasets/wildfire/wildfireSubSampler.py
+++ pyronear/datasets/wildfire/wildfireSubSampler.py
@@ -11,9 +11,34 @@ import random
 import PIL
 
 
-class SubSamplerDataset(Dataset):
+class wildfireSubSampler(Dataset):
+    """WildFireSubSampler dataset that can be fed to a torch model
 
-    def __init__(self, metadata, path_to_frames, frame_per_seq, target_names=None, train=False, transform=None):
+    Parameters
+    ----------
+    metadata: str or Pandas.DataFrame
+        Path leading to a CSV that will contain the metada of the dataset or directly the DataFrame.
+        Field that should be present:
+            'imgFile': path_to_frame
+            'fire_id': fire index
+
+    target_names: list,
+        List of the columns that can be found in metadata CSV and that represent targets
+        we want to return when accessing the datasets
+        If left to None, will be set to ['fire']
+        Example: ['fire', 'clf_confidence', 'loc_confidence', 'x', 'y']
+
+    path_to_frames: str
+        Path leading to the directory containing the frames referenced in metadata 'imgFile':
+
+    frame_per_seq: int
+        Number of frames to take for each sequence
+
+    transform: object, optional
+        Transformations to apply to the frames (ie: torchvision.transforms)
+    """
+
+    def __init__(self, metadata, path_to_frames, frame_per_seq, target_names=None, transform=None):
         if isinstance(metadata, pd.DataFrame):
             self.metadata = metadata
         else:
@@ -26,67 +51,77 @@ class SubSamplerDataset(Dataset):
         self.target_names = target_names or ['fire']
         self.transform = transform
         self.frame_per_seq = frame_per_seq
-        metadata.index = np.arange(len(metadata))
-        self.metadata = metadata
-        self.seq = metadata['seq']
+        self.metadata.index = np.arange(len(self.metadata))
+        #Get unique list of sequences
+        self.seq = self.metadata['seq']
         my_set = set(self.seq)
         self.uniqueSEQ = list(my_set)
         random.shuffle(self.uniqueSEQ)
-        self.imgs = metadata['imgFile']
+        self.imgs = self.metadata['imgFile']
         self.SubSetImgs = []
+        self.probTh = None
 
-        self.lenSubSet = 0
-        self.train = train
+    def computeSubSet(self, probTh=None):
+        """This function computes the subset, it extracts frame_per_seq frames for each sequence.
 
-    def computeSubSet(self, bs):
+        Parameters
+        ----------
+        probTh: float , optional
+            The data set contains many more frames classified 'fire' than 'not fire', this parameter
+            allows to equalize the dataset. For each 'not fire' sequence, we draw a random number if
+            it is greater than probTh we double the number of frames used for this sequence
+        """
         meta = self.metadata
-
         self.SubSetImgs = []
+        self.probTh = probTh
+        SubSetImgsEq = []
 
         for seU in self.uniqueSEQ:
+            #For each sequence get a subSample of frame_per_seq frames
             nn = [self.imgs[i] for i, se in enumerate(self.seq) if se == seU]
             if(len(nn) > self.frame_per_seq):
                 nn = random.sample(nn, self.frame_per_seq)
             nb = [float(frame.split("frame", 1)[1].split(".", 1)[0]) for frame in nn]
             nb, nn = (list(t) for t in zip(*sorted(zip(nb, nn))))
             self.SubSetImgs += nn
-            if(meta[meta['seq'] == seU]['fire'].values[0] == 0 and random.randint(0, 1) == 1):
-                nn = [self.imgs[i] for i, se in enumerate(self.seq) if se == seU]
-                if(len(nn) > self.frame_per_seq):
-                    nn = random.sample(nn, self.frame_per_seq)
-                nb = [float(frame.split("frame", 1)[1].split(".", 1)[0]) for frame in nn]
-                nb, nn = (list(t) for t in zip(*sorted(zip(nb, nn))))
-                self.SubSetImgs += nn
+            #Equalize the dataset adding not_fire frames
+            if probTh is not None:
+                if(meta[meta['seq'] == seU]['fire'].values[0] == 0 and random.random() > probTh):
+                    nn = [self.imgs[i] for i, se in enumerate(self.seq) if se == seU]
+                    if(len(nn) > self.frame_per_seq):
+                        nn = random.sample(nn, self.frame_per_seq)
+                    nb = [float(frame.split("frame", 1)[1].split(".", 1)[0]) for frame in nn]
+                    nb, nn = (list(t) for t in zip(*sorted(zip(nb, nn))))
+                    SubSetImgsEq += nn
+
+        #Insert randomly the extra frames in the dataset
+        if probTh is not None:
+            for i in range(0, len(SubSetImgsEq), 2):
+                idx = random.randint(0, len(self.SubSetImgs) - 2) // 2 * 2
+                self.SubSetImgs.insert(idx, SubSetImgsEq[i + 1])
+                self.SubSetImgs.insert(idx, SubSetImgsEq[i])
 
     def __len__(self):
         return len(self.SubSetImgs)
 
-    def __repr__(self):
-        return f'{super().__repr__()}\nPath: {self.path}'
-
-    def get(self, i):
-        return i
-
     def __getitem__(self, index):
-
         img = self.SubSetImgs[index]
+        #get the corresponding metadata
         meta = self.metadata[self.metadata['imgFile'] == img]
-
         path_to_frame = self.path_to_frames / str(meta['imgFile'].values[0])
-
         observation = PIL.Image.open(path_to_frame)
 
         if self.transform:
             observation = self.transform(observation)
 
         #return observation, meta.values
-        return observation, meta['fire'].values[0]
+        return observation, meta[self.target_names].values[0]
 
 
 class WildFireSplitter:
-    """Split one WildFireDataset into train, validation and test sets
+    """Split one wildfireSubSampler into train, validation and test sets
 
-    Three WildFireDataset instances will be created according the the given ratios.
+    Three wildfireSubSampler instances will be created according the the given ratios.
     At this time, it is recommanded to transmit the transforms for each set.
     To avoid leakage in test and val test, splitting preserve fire_id consistency
     (ie: train set will only contain frames recorded before the ones contained in test set)
@@ -113,8 +148,8 @@ class WildFireSplitter:
 
     Example
     -------
-    wildfire = WildFireDataset(metadata='wildfire.csv', path_to_frames=path_to_frames)
-
+    wildfire = wildfireSubSampler(metadata=metadata,path_to_frames=path_to_frames,frame_per_seq=K,target_names=['fire'])
+    wildfire.computeSubSet(0)
     ratios = {'train': 0.7, 'val': 0.15, 'test':0.15}
     splitter =  WildFireSplitter(ratios)
     splitter.fit(wildfire)
@@ -158,20 +193,14 @@ class WildFireSplitter:
     def test(self):
         return self.splits['test']
 
-    def fit(self, wildfire, batch_size):
+    def fit(self, wildfireSS):
         """Split the wildfire dataset according to the given ratios.
 
         Set splits attribute
         Set also estimated posterior ratio(ratio_train_, ratio_val_ and ratio_test_)
         Because split is randomly done
         """
-        self.wildfire = wildfire
-        self.batch_size = batch_size
-        # Some checks first
-        # if wildfire.metadata['fire_id'].nunique() != wildfire.metadata['fire_id'].max() + 1:
-        #     warnings.warn(f"Inconsistent Fire Labeling. Maybe try to label the fire again\n"
-        #                   f"Distinct values of ids({wildfire.metadata['fire_id'].nunique()}"
-        #                   f" ≠ {wildfire.metadata['fire_id'].max() + 1})", Warning)
+        self.wildfireSS = wildfireSS
 
         if self.algorithm != 'auto':
             raise ValueError(f"Algorithm {self.algorithm} is unknown. Only 'auto' available for now")
@@ -180,11 +209,11 @@ class WildFireSplitter:
 
         # Let's split
         strategy = self._strategy()
-        dataframes = strategy.split(wildfire, self.ratios, self.seed)
+        dataframes = strategy.split(self.wildfireSS, self.ratios, self.seed)
         self.set_splits(dataframes)
 
     def set_splits(self, dataframes):
-        """Instantiate the Split as WildFireDataset and define the estimated parameters
+        """Instantiate the Split as wildfireSubSampler and define the estimated parameters
 
         Parameters
         ----------
@@ -193,14 +222,14 @@ class WildFireSplitter:
             It should have 'train', 'val' and 'test' as keys.
         """
         for set_ in ['train', 'val', 'test']:
-            self.splits[set_] = SubSamplerDataset(metadata=dataframes[set_],
-                                                  path_to_frames=self.wildfire.path_to_frames,
-                                                  target_names=self.wildfire.target_names,
-                                                  frame_per_seq=self.wildfire.frame_per_seq,
-                                                  transform=self.transforms[set_])
+            self.splits[set_] = wildfireSubSampler(metadata=dataframes[set_],
+                                                   path_to_frames=self.wildfireSS.path_to_frames,
+                                                   target_names=self.wildfireSS.target_names,
+                                                   frame_per_seq=self.wildfireSS.frame_per_seq,
+                                                   transform=self.transforms[set_])
 
-            self.splits[set_].computeSubSet(self.batch_size)
+            self.splits[set_].computeSubSet(self.wildfireSS.probTh)
 
         # Determine estimated(posterior) parameters
         self.n_samples_ = {set_: len(self.splits[set_]) for set_ in ['train', 'val', 'test']}
-        self.ratios_ = {set_: (self.n_samples_[set_] / len(self.wildfire)) for set_ in ['train', 'val', 'test']}
+        self.ratios_ = {set_: (self.n_samples_[set_] / len(self.wildfireSS)) for set_ in ['train', 'val', 'test']}
