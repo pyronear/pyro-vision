19dba957b53291e8368fb2fd8fe888af18a432b6
matéo lostanlen <mateo.lostanlen@gmail.com>
2020-05-17 17:09:00 +0200

    Add test for SSResNet18

--- pyronear/models/ssresnet18.py
+++ pyronear/models/ssresnet18.py
@@ -5,9 +5,8 @@ import torch.nn.functional as F
 
 
 class SSResNet18(ResNet):
-    def __init__(self, bs, frame_per_seq):
+    def __init__(self, frame_per_seq):
         super(SSResNet18, self).__init__(BasicBlock, [2, 2, 2, 2])
-        self.bs = bs
         self.frame_per_seq = frame_per_seq
         self.fc = nn.Linear(int(512 * self.frame_per_seq), 32)
         self.fc2 = nn.Linear(32, 2)
@@ -33,8 +32,8 @@ class SSResNet18(ResNet):
         x = F.relu(self.fc(x))
         x = self.fc2(x)
         # reshape to expexted output
-        x2 = torch.cat([x] * frame_per_seq)
-        for i in range(frame_per_seq):
+        x2 = torch.cat([x] * self.frame_per_seq)
+        for i in range(self.frame_per_seq):
             x2[i::self.frame_per_seq] = x
 
         return x2
