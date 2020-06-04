e0a5954b064ae63f2ac4c44ef6f25e475dc36e29
F-G Fernandez <fgfm03@hotmail.fr>
2020-06-04 15:44:50 +0200

    refactor: Reverted back to assert syntax

--- pyronear/datasets/wildfire/fire_labeler.py
+++ pyronear/datasets/wildfire/fire_labeler.py
@@ -109,8 +109,7 @@ class FireLabeler:
 
         length = len(self.fire_ids[self.fire_ids == -1])
         self.fire_ids[self.fire_ids == -1] = range(current_fire_id, current_fire_id + length)
-        if self.fire_ids[self.fire_ids == -1].sum() != 0:
-            raise AssertionError("Singletons escaped indexation!")
+        assert self.fire_ids[self.fire_ids == -1].sum() == 0, "Singletons escaped indexation!"
         return self
 
     @staticmethod
