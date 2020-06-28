397249ea5abe8421da294086e7037105c4873d51
matéo lostanlen <mateo.lostanlen@gmail.com>
2020-06-28 23:00:35 +0200

    fix typo

--- inference/inference.py
+++ inference/inference.py
@@ -13,7 +13,7 @@ from torchvision import transforms
 
 
 class PyronearPredictor:
-    """This class use the last pyronear model to predict if a sigle frame contain a fire or not 
+    """This class use the last pyronear model to predict if a sigle frame contain a fire or not
 
     Example
     -------
@@ -25,31 +25,29 @@ class PyronearPredictor:
     """
     def __init__(self):
         # Model definition
-		self.model = torchvision.models.resnet18(pretrained=True)
+        self.model = torchvision.models.resnet18(pretrained=True)
 
-		# Change fc
-		in_features = getattr(self.model, 'fc').in_features
-		setattr(self.model, 'fc', nn.Linear(in_features, 2))
+        # Change fc
+        in_features = getattr(self.model, 'fc').in_features
+        setattr(self.model, 'fc', nn.Linear(in_features, 2))
 
-		self.model.load_state_dict(torch.load("model/model_resnet18.txt"))
+        self.model.load_state_dict(torch.load("model/model_resnet18.txt"))
 
-		normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-		self.tf = transforms.Compose([
-		    transforms.Resize((224,224)),
-		    transforms.ToTensor(),
-		    normalize
-		])
+        self.tf = transforms.Compose([transforms.Resize((224, 224)),
+                                      transforms.ToTensor(),
+                                      normalize])
 
     def predict(self, imPath):
         im = Image.open(imPath)
-		imT = self.tf(im)
+        imT = self.tf(im)
 
-		model.eval()
-		with torch.no_grad():
-		    pred = self.model(imT.unsqueeze(0))
-		    
-		if pred[0,0]>pred[0,1]:
-		    return "no fire"
-		else:
-		    return "FIRE !!!"
+        self.model.eval()
+        with torch.no_grad():
+            pred = self.model(imT.unsqueeze(0))
+
+        if pred[0, 0] > pred[0, 1]:
+            return "no fire"
+        else:
+            return "FIRE !!!"
