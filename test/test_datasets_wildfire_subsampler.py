6ab8d03f776e414b0a8b1def6f22dd08f64f995c
matéo lostanlen <mateo.lostanlen@gmail.com>
2020-06-28 22:17:30 +0200

    fix license header

--- test/test_datasets_wildfire_subsampler.py
+++ test/test_datasets_wildfire_subsampler.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
+
 # Copyright (c) Pyronear contributors.
 # This file is dual licensed under the terms of the CeCILL-2.1 and AGPLv3 licenses.
-# See the LICENSE file in the root of this repository for complete details.import unittest
+# See the LICENSE file in the root of this repository for complete details.
 
 from pathlib import Path
 import pandas as pd
