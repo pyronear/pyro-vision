6850ec1dd762db4066ba6110b58b4c55d2902cfd
matéo lostanlen <mateo.lostanlen@gmail.com>
2020-06-29 16:36:36 +0200

    put back import unitest

--- test/test_datasets_wildfire_subsampler.py
+++ test/test_datasets_wildfire_subsampler.py
@@ -4,6 +4,7 @@
 # This file is dual licensed under the terms of the CeCILL-2.1 and AGPLv3 licenses.
 # See the LICENSE file in the root of this repository for complete details.
 
+import unittest
 from pathlib import Path
 import pandas as pd
 
