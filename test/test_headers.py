8c6267b6de6cb3f402828b99949c59791339bf48
frgfm <fgfm03@hotmail.fr>
2021-01-31 14:35:59 +0100

    test: Updated header unittest

--- test/test_headers.py
+++ test/test_headers.py
@@ -10,40 +10,39 @@ from pathlib import Path
 class HeadersTester(unittest.TestCase):
 
     def setUp(self):
-        self.header_shebang = "#!usr/bin/python\n"
-        self.header_coding = "# -*- coding: utf-8 -*-\n"
-        self.header_blank = "\n"
+        shebang = ["#!usr/bin/python\n"]
+        blank_line = "\n"
 
-        self.header_notice = ("# Copyright (c) Pyronear contributors.\n"
-                              "# This file is dual licensed under the terms of the CeCILL-2.1 and AGPLv3 licenses.\n"
-                              "# See the LICENSE file in the root of this repository for complete details.\n")
+        copyright_notice = ["# Copyright (C) 2021, Pyronear contributors.\n"]
+        license_notice = [
+            "# This program is licensed under the GNU Affero General Public License version 3.\n",
+            "# See LICENSE or go to <https://www.gnu.org/licenses/agpl-3.0.txt> for full license details.\n"
+        ]
 
-        self.excluded_files = ["version.py"]
+        self.headers = [
+            shebang + [blank_line] + copyright_notice + [blank_line] + license_notice,
+            copyright_notice + [blank_line] + license_notice
+        ]
 
-    def test_first_two_lines_are_valid(self):
+        self.excluded_files = ["version.py", "__init__.py"]
+
+
+    def test_headers(self):
         # For every python file in the repository
         for source_path in Path(__file__).parent.parent.rglob('*.py'):
             if source_path.name not in self.excluded_files:
-                with open(source_path) as source_file:
-                    try:
-                        first_two_lines = (next(source_file), next(source_file))
-                    except StopIteration:
-                        raise ValueError(f"Less than two lines in {source_path}. cannot check for shebang/encoding")
+                # Parse header
+                header_length = max(len(option) for option in self.headers)
+                current_header = []
+                with open(source_path) as f:
+                    for idx, line in enumerate(f):
+                        current_header.append(line)
+                        if idx == header_length - 1:
+                            break
 
-                    error_msg = f"\nInvalid first two lines in {source_path}"
-                    self.assertIn(first_two_lines, [(self.header_shebang, self.header_coding),
-                                                    (self.header_coding, self.header_blank)], msg=error_msg)
-
-    def test_license_headers(self):
-        """Test if license headers are correctly added at beginning of each file"""
-        for source_path in Path(__file__).parent.parent.rglob('*.py'):
-            if source_path.name not in self.excluded_files:
-                with open(source_path) as source_file:
-                    first_six_lines = ''.join(next(source_file) for _ in range(6))
-
-                    error_msg = (f"\nHeader notice:\n{self.header_notice}\n"
-                                 f"not found in first six lines of {source_path}:\n{first_six_lines}\n")
-                    self.assertTrue(self.header_notice in first_six_lines, msg=error_msg)
+                # Compare it
+                self.assertTrue(any("".join(current_header[:min(len(option), len(current_header))]) == "".join(option)
+                                    for option in self.headers), msg=f"Invalid header in {source_path}")
 
 
 if __name__ == '__main__':
