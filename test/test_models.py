b0c4cfc3636edb0c2f21426b41e7be368a6ca46a
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2021-02-06 10:33:35 +0100

    keep change

--- test/test_models.py
+++ test/test_models.py
@@ -13,7 +13,6 @@ from torchvision.models.resnet import BasicBlock
 from PIL import Image
 from pathlib import Path
 from torchvision import transforms
-import inspect
 
 
 def set_rng_seed(seed):
@@ -70,18 +69,16 @@ class ModelsTester(unittest.TestCase):
         num_classes = 50
 
         # Pretrained parameters
-        args = inspect.getfullargspec(models.__dict__[name])
-        if 'imagenet_pretrained' in args.args:
-            self.assertRaises(ValueError or TypeError, models.__dict__[name], pretrained=True, imagenet_pretrained=True)
+        self.assertRaises(ValueError, models.__dict__[name], pretrained=True, imagenet_pretrained=True)
 
-            # Default case
-            model = models.__dict__[name](num_classes=num_classes)
-            model.eval()
-            x = torch.rand(input_shape)
-            with torch.no_grad():
-                out = model(x)
-            # self.assertExpected(out, rtol=1e-2, atol=0.)
-            self.assertEqual(out.shape[-1], 50)
+        # Default case
+        model = models.__dict__[name](num_classes=num_classes)
+        model.eval()
+        x = torch.rand(input_shape)
+        with torch.no_grad():
+            out = model(x)
+        # self.assertExpected(out, rtol=1e-2, atol=0.)
+        self.assertEqual(out.shape[-1], 50)
 
     def test_ssresnet_model(self):
 
@@ -145,12 +142,15 @@ class ModelsTester(unittest.TestCase):
 for model_name in get_available_classification_models():
     # for-loop bodies don't define scopes, so we have to save the variables
     # we want to close over in some way
-    def do_test(self, model_name=model_name):
-        if model_name != '..pyronear_model':
-            input_shape = (1, 3, 224, 224)
-            self._test_classification_model(model_name, input_shape)
+    if model_name != "pyronear_model":
+        def do_test(self, model_name=model_name):
+            print(model_name, type(model_name), model_name != '..pyronear_model')
+            if model_name != "..pyronear_model":
+                #print(model_name, type(model_name), model_name != '..pyronear_model')
+                input_shape = (1, 3, 224, 224)
+                self._test_classification_model(model_name, input_shape)
 
-    setattr(ModelsTester, "test_" + model_name, do_test)
+        setattr(ModelsTester, "test_" + model_name, do_test)
 
 
 if __name__ == '__main__':
