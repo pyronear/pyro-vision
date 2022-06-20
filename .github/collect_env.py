5a48098c22d5733c7e91d31a1049ca19943c89d6
frgfm <26927750+frgfm@users.noreply.github.com>
2022-06-20 13:04:26 +0200

    ci: Moved environement collection script

--- .github/collect_env.py
+++ .github/collect_env.py
@@ -20,18 +20,21 @@ from collections import namedtuple
 
 try:
     import pyrovision
+
     PYROVISION_AVAILABLE = True
 except (ImportError, NameError, AttributeError):
     PYROVISION_AVAILABLE = False
 
 try:
     import torch
+
     TORCH_AVAILABLE = True
 except (ImportError, NameError, AttributeError):
     TORCH_AVAILABLE = False
 
 try:
     import torchvision
+
     VISION_AVAILABLE = True
 except (ImportError, NameError, AttributeError):
     VISION_AVAILABLE = False
@@ -40,24 +43,26 @@ PY3 = sys.version_info >= (3, 0)
 
 
 # System Environment Information
-SystemEnv = namedtuple('SystemEnv', [
-    'pyrovision_version',
-    'torch_version',
-    'torchvision_version',
-    'os',
-    'python_version',
-    'is_cuda_available',
-    'cuda_runtime_version',
-    'nvidia_driver_version',
-    'nvidia_gpu_models',
-    'cudnn_version',
-])
+SystemEnv = namedtuple(
+    "SystemEnv",
+    [
+        "pyrovision_version",
+        "torch_version",
+        "torchvision_version",
+        "os",
+        "python_version",
+        "is_cuda_available",
+        "cuda_runtime_version",
+        "nvidia_driver_version",
+        "nvidia_gpu_models",
+        "cudnn_version",
+    ],
+)
 
 
 def run(command):
     """Returns (return-code, stdout, stderr)"""
-    p = subprocess.Popen(command, stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE, shell=True)
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, err = p.communicate()
     rc = p.returncode
     if PY3:
@@ -87,53 +92,52 @@ def run_and_parse_first_match(run_lambda, command, regex):
 
 
 def get_nvidia_driver_version(run_lambda):
-    if get_platform() == 'darwin':
-        cmd = 'kextstat | grep -i cuda'
-        return run_and_parse_first_match(run_lambda, cmd,
-                                         r'com[.]nvidia[.]CUDA [(](.*?)[)]')
+    if get_platform() == "darwin":
+        cmd = "kextstat | grep -i cuda"
+        return run_and_parse_first_match(run_lambda, cmd, r"com[.]nvidia[.]CUDA [(](.*?)[)]")
     smi = get_nvidia_smi()
-    return run_and_parse_first_match(run_lambda, smi, r'Driver Version: (.*?) ')
+    return run_and_parse_first_match(run_lambda, smi, r"Driver Version: (.*?) ")
 
 
 def get_gpu_info(run_lambda):
-    if get_platform() == 'darwin':
+    if get_platform() == "darwin":
         if TORCH_AVAILABLE and torch.cuda.is_available():
             return torch.cuda.get_device_name(None)
         return None
     smi = get_nvidia_smi()
-    uuid_regex = re.compile(r' \(UUID: .+?\)')
-    rc, out, _ = run_lambda(smi + ' -L')
+    uuid_regex = re.compile(r" \(UUID: .+?\)")
+    rc, out, _ = run_lambda(smi + " -L")
     if rc != 0:
         return None
     # Anonymize GPUs by removing their UUID
-    return re.sub(uuid_regex, '', out)
+    return re.sub(uuid_regex, "", out)
 
 
 def get_running_cuda_version(run_lambda):
-    return run_and_parse_first_match(run_lambda, 'nvcc --version', r'release .+ V(.*)')
+    return run_and_parse_first_match(run_lambda, "nvcc --version", r"release .+ V(.*)")
 
 
 def get_cudnn_version(run_lambda):
     """This will return a list of libcudnn.so; it's hard to tell which one is being used"""
-    if get_platform() == 'win32':
+    if get_platform() == "win32":
         cudnn_cmd = 'where /R "%CUDA_PATH%\\bin" cudnn*.dll'
-    elif get_platform() == 'darwin':
+    elif get_platform() == "darwin":
         # CUDA libraries and drivers can be found in /usr/local/cuda/. See
         # https://docs.nvidia.com/cuda/cuda-installation-guide-mac-os-x/index.html#install
         # https://docs.nvidia.com/deeplearning/sdk/cudnn-install/index.html#installmac
         # Use CUDNN_LIBRARY when cudnn library is installed elsewhere.
-        cudnn_cmd = 'ls /usr/local/cuda/lib/libcudnn*'
+        cudnn_cmd = "ls /usr/local/cuda/lib/libcudnn*"
     else:
         cudnn_cmd = 'ldconfig -p | grep libcudnn | rev | cut -d" " -f1 | rev'
     rc, out, _ = run_lambda(cudnn_cmd)
     # find will return 1 if there are permission errors or if not found
     if len(out) == 0 or rc not in (1, 0):
-        lib = os.environ.get('CUDNN_LIBRARY')
+        lib = os.environ.get("CUDNN_LIBRARY")
         if lib is not None and os.path.isfile(lib):
             return os.path.realpath(lib)
         return None
     files = set()
-    for fn in out.split('\n'):
+    for fn in out.split("\n"):
         fn = os.path.realpath(fn)  # eliminate symbolic links
         if os.path.isfile(fn):
             files.add(fn)
@@ -143,61 +147,60 @@ def get_cudnn_version(run_lambda):
     files = list(sorted(files))
     if len(files) == 1:
         return files[0]
-    result = '\n'.join(files)
-    return 'Probably one of the following:\n{}'.format(result)
+    result = "\n".join(files)
+    return "Probably one of the following:\n{}".format(result)
 
 
 def get_nvidia_smi():
     # Note: nvidia-smi is currently available only on Windows and Linux
-    smi = 'nvidia-smi'
-    if get_platform() == 'win32':
+    smi = "nvidia-smi"
+    if get_platform() == "win32":
         smi = '"C:\\Program Files\\NVIDIA Corporation\\NVSMI\\%s"' % smi
     return smi
 
 
 def get_platform():
-    if sys.platform.startswith('linux'):
-        return 'linux'
-    elif sys.platform.startswith('win32'):
-        return 'win32'
-    elif sys.platform.startswith('cygwin'):
-        return 'cygwin'
-    elif sys.platform.startswith('darwin'):
-        return 'darwin'
+    if sys.platform.startswith("linux"):
+        return "linux"
+    elif sys.platform.startswith("win32"):
+        return "win32"
+    elif sys.platform.startswith("cygwin"):
+        return "cygwin"
+    elif sys.platform.startswith("darwin"):
+        return "darwin"
     else:
         return sys.platform
 
 
 def get_mac_version(run_lambda):
-    return run_and_parse_first_match(run_lambda, 'sw_vers -productVersion', r'(.*)')
+    return run_and_parse_first_match(run_lambda, "sw_vers -productVersion", r"(.*)")
 
 
 def get_windows_version(run_lambda):
-    return run_and_read_all(run_lambda, 'wmic os get Caption | findstr /v Caption')
+    return run_and_read_all(run_lambda, "wmic os get Caption | findstr /v Caption")
 
 
 def get_lsb_version(run_lambda):
-    return run_and_parse_first_match(run_lambda, 'lsb_release -a', r'Description:\t(.*)')
+    return run_and_parse_first_match(run_lambda, "lsb_release -a", r"Description:\t(.*)")
 
 
 def check_release_file(run_lambda):
-    return run_and_parse_first_match(run_lambda, 'cat /etc/*-release',
-                                     r'PRETTY_NAME="(.*)"')
+    return run_and_parse_first_match(run_lambda, "cat /etc/*-release", r'PRETTY_NAME="(.*)"')
 
 
 def get_os(run_lambda):
     platform = get_platform()
 
-    if platform in ('win32', 'cygwin'):
+    if platform in ("win32", "cygwin"):
         return get_windows_version(run_lambda)
 
-    if platform == 'darwin':
+    if platform == "darwin":
         version = get_mac_version(run_lambda)
         if version is None:
             return None
-        return 'Mac OSX {}'.format(version)
+        return "Mac OSX {}".format(version)
 
-    if platform == 'linux':
+    if platform == "linux":
         # Ubuntu/Debian based
         desc = get_lsb_version(run_lambda)
         if desc is not None:
@@ -220,24 +223,24 @@ def get_env_info():
     if PYROVISION_AVAILABLE:
         pyrovision_str = pyrovision.__version__
     else:
-        pyrovision_str = 'N/A'
+        pyrovision_str = "N/A"
 
     if TORCH_AVAILABLE:
         torch_str = torch.__version__
         cuda_available_str = torch.cuda.is_available()
     else:
-        torch_str = cuda_available_str = 'N/A'
+        torch_str = cuda_available_str = "N/A"
 
     if VISION_AVAILABLE:
         torchvision_str = torchvision.__version__
     else:
-        torchvision_str = 'N/A'
+        torchvision_str = "N/A"
 
     return SystemEnv(
         pyrovision_version=pyrovision_str,
         torch_version=torch_str,
         torchvision_version=torchvision_str,
-        python_version='{}.{}'.format(sys.version_info[0], sys.version_info[1]),
+        python_version="{}.{}".format(sys.version_info[0], sys.version_info[1]),
         is_cuda_available=cuda_available_str,
         cuda_runtime_version=get_running_cuda_version(run_lambda),
         nvidia_gpu_models=get_gpu_info(run_lambda),
@@ -264,14 +267,14 @@ cuDNN version: {cudnn_version}
 
 
 def pretty_str(envinfo):
-    def replace_nones(dct, replacement='Could not collect'):
+    def replace_nones(dct, replacement="Could not collect"):
         for key in dct.keys():
             if dct[key] is not None:
                 continue
             dct[key] = replacement
         return dct
 
-    def replace_bools(dct, true='Yes', false='No'):
+    def replace_bools(dct, true="Yes", false="No"):
         for key in dct.keys():
             if dct[key] is True:
                 dct[key] = true
@@ -281,28 +284,26 @@ def pretty_str(envinfo):
 
     def maybe_start_on_next_line(string):
         # If `string` is multiline, prepend a \n to it.
-        if string is not None and len(string.split('\n')) > 1:
-            return '\n{}\n'.format(string)
+        if string is not None and len(string.split("\n")) > 1:
+            return "\n{}\n".format(string)
         return string
 
     mutable_dict = envinfo._asdict()
 
     # If nvidia_gpu_models is multiline, start on the next line
-    mutable_dict['nvidia_gpu_models'] = \
-        maybe_start_on_next_line(envinfo.nvidia_gpu_models)
+    mutable_dict["nvidia_gpu_models"] = maybe_start_on_next_line(envinfo.nvidia_gpu_models)
 
     # If the machine doesn't have CUDA, report some fields as 'No CUDA'
     dynamic_cuda_fields = [
-        'cuda_runtime_version',
-        'nvidia_gpu_models',
-        'nvidia_driver_version',
+        "cuda_runtime_version",
+        "nvidia_gpu_models",
+        "nvidia_driver_version",
     ]
-    all_cuda_fields = dynamic_cuda_fields + ['cudnn_version']
-    all_dynamic_cuda_fields_missing = all(
-        mutable_dict[field] is None for field in dynamic_cuda_fields)
+    all_cuda_fields = dynamic_cuda_fields + ["cudnn_version"]
+    all_dynamic_cuda_fields_missing = all(mutable_dict[field] is None for field in dynamic_cuda_fields)
     if TORCH_AVAILABLE and not torch.cuda.is_available() and all_dynamic_cuda_fields_missing:
         for field in all_cuda_fields:
-            mutable_dict[field] = 'No CUDA'
+            mutable_dict[field] = "No CUDA"
 
     # Replace True with Yes, False with No
     mutable_dict = replace_bools(mutable_dict)
@@ -328,5 +329,5 @@ def main():
     print(output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
